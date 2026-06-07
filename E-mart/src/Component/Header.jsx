536ca20f60eaf02e2b3489ff1c0c6e0a432708f4
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-06-07 13:21:36 +0530

    ..

--- E-mart/src/Component/Header.jsx
+++ E-mart/src/Component/Header.jsx
@@ -6,6 +6,14 @@ function Header() {
       <div>
         <h1>E-Mart</h1>
       </div>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/products">Products</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
     </header>
     
    )

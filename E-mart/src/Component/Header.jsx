7adf8debdbcef5596df3dbaeff22389c0a43a520
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-06-07 13:29:36 +0530

    nav

--- E-mart/src/Component/Header.jsx
+++ E-mart/src/Component/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Header() {
   return (
-    <header className="">
-      <div>
+    <header className="bg-gray-800 text-white flex justify-between">
+      <div className="text-3xl font-bold flex items-center">
         <h1>E-Mart</h1>
       </div>
       <nav>

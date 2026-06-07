6d9b599740180fe7701c0d72a3062fb2a1175e37
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-06-07 13:22:51 +0530

    ..

--- E-mart/src/App.jsx
+++ E-mart/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Header from './Component/Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>e-mart</h1>
+     <Header/>
     </>
   )
 }

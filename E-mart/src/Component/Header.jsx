4b2dc421ce668ae20d4da495d926bc28b1fb246a
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-06-07 15:07:42 +0530

    commit

--- E-mart/src/Component/Header.jsx
+++ E-mart/src/Component/Header.jsx
@@ -3,7 +3,19 @@ import { FaCartArrowDown } from "react-icons/fa";
 
 function Header() {
   return (
-    <></>
+   <nav className='bg-gray-800 text-white p-2 flex justify-between items-center'>
+    <div className='flex items-center space-x-2'>
+      <FaCartArrowDown />
+      <h1>E-Mart</h1>
+    </div>
+    <ul className='flex space-x-4'>
+      <li className='hover:text-gray-300'><a href="/">Home</a></li>
+      <li className='hover:text-gray-300'><a href="/products">Products</a></li>
+      <li className='hover:text-gray-300'><a href="/about">About</a></li>
+      <li className='hover:text-gray-300'><a href="/contact">Contact</a></li>
+    </ul>
+
+   </nav>
     
    )
 }

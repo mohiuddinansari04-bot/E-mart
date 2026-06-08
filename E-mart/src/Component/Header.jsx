933f4cb4c8c6156e489368754fe4db7030cfb5da
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-06-09 01:11:03 +0530

    main

--- E-mart/src/Component/Header.jsx
+++ E-mart/src/Component/Header.jsx
@@ -2,26 +2,24 @@ import React from "react";
 import { FaCartArrowDown } from "react-icons/fa";
 import { RiShoppingCart2Line } from "react-icons/ri";
 
-
 function Header() {
   return (
-   <nav className="bg-white shadow-lg p-4">
-    <div className="max-w-7xl max-auto flex justify-between items-center">
-    
-      <div className="flex items-center space-x-2"> {/* Logo & heading */}
-        <FaCartArrowDown/>
-        <h1 className="text-xl font-bold">E-Mart</h1>
+    <nav className="bg-white shadow-lg p-4">
+      <div className="max-w-9xl max-auto flex justify-between items-center md:flex-row sm:flex-col">
+        <div className="flex items-center space-x-2">
+          {" "}
+          {/* Logo & heading */}
+          <FaCartArrowDown />
+          <h1 className="text-xl font-bold">E-Mart</h1>
+        </div>
+
+       
+        <button className="flex items-center space-x-1 bg-blue-500 text-white px-4  py-2 rounded hover:bg-blue-600 transition duration-300">
+           <RiShoppingCart2Line/>
+           <span className="sm:inline ">Cart</span>
+        </button>
       </div>
-      <Div>
-      <RiShoppingCart2Line />
-      <button className="bg">
-        Cart
-      </button>
-      </Div>
-
-    </div>
-
-   </nav>
+    </nav>
   );
 }
 
